d297300364ee5f02d1385e48fae743a014811aac
Filipe Andrade <filipeandrade.work@gmail.com>
2023-01-18 19:14:42 -0300

    designed with css and starting to do backend

--- src/main/java/javafx/modelo/MainController.java
+++ src/main/java/javafx/modelo/MainController.java
@@ -1,8 +1,68 @@
 package javafx.modelo;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private Button bt_add_menu;
+
+    @FXML
+    private Button bt_config_menu;
+
+    @FXML
+    private Button bt_home_menu;
+
+    @FXML
+    private Button bt_tasks_menu;
+
+    @FXML
+    private Pane pane_add;
+
+    @FXML
+    private Pane pane_config;
+
+    @FXML
+    private Pane pane_home;
+
+    @FXML
+    private Pane pane_tasks;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        bt_add_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                pane_add.toFront();
+            }
+        });
+        bt_config_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                pane_config.toFront();
+            }
+        });
+        bt_tasks_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                pane_tasks.toFront();
+            }
+        });
+        bt_home_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                pane_home.toFront();
+            }
+        });
+    }
 
 }
