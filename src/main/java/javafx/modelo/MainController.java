d73f321d10e24de0f90952c2cc2714706e3fa45a
Filipe Andrade <filipeandrade.work@gmail.com>
2023-01-22 20:25:40 -0300

    working change of pages

--- src/main/java/javafx/modelo/MainController.java
+++ src/main/java/javafx/modelo/MainController.java
@@ -5,9 +5,11 @@ import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,7 +25,71 @@ public class MainController implements Initializable {
      * organized in the controller */
 
     /* FXML VARIABLES MADE BY SCENEBUILDER for example:*/
+
+    @FXML
+    private AnchorPane anchorpane;
+
+    @FXML
+    private Button bt_config_menu;
+
+    @FXML
+    private Button bt_home_menu;
+
+    @FXML
+    private Button bt_projects_menu;
+
+    @FXML
+    private Button bt_tasks_menu;
+
+    @FXML
+    private Label label_andrade;
+
+    @FXML
+    private VBox vbox_config;
+
+    @FXML
+    private VBox vbox_home;
+
+    @FXML
+    private VBox vbox_menu;
+
+    @FXML
+    private VBox vbox_projects;
+
+    @FXML
+    private VBox vbox_tasks;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        /* HOME BUTTON */
+        bt_home_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                vbox_home.toFront();
+            }
+        });
+        /* TASKS BUTTON */
+        bt_tasks_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                vbox_tasks.toFront();
+            }
+        });
+        /* PROJECTS BUTTON */
+        bt_projects_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                vbox_projects.toFront();
+            }
+        });
+        /* CONFIG BUTTON */
+        bt_config_menu.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                vbox_config.toFront();
+            }
+        });
+
+
     }
 }
