6f241ff06bbe30b5cf81a33ad6a10deb87a50580
Filipe Andrade <filipeandrade.work@gmail.com>
2023-02-17 15:57:49 -0300

    Working on the Improvements, starting with the Style, changed the menu location and buttons

--- src/main/java/javafx/modelo/DataBase.java
+++ src/main/java/javafx/modelo/DataBase.java
@@ -8,7 +8,6 @@ public class DataBase {
     /*The Objective of this class is connect the program to the database
     * Database Located at(in my case, local machine):
     * example: "jdbc:mysql://localhost:3306/task_manager", "root", "" */
-
     static final String db_Url = "jdbc:mysql://localhost:3306/task_manager";
     static final String db_User = "root";
     static final String db_Password = "";
